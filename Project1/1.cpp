8025005893b6f08d12c660e2acc98fb63a6ab23f
Owner <Owner@DESKTOP-A1LQET1>
2023-07-22 15:49:53 +0900

    초기 커밋.

---
+++ Project1/1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// 자료형 : Data Type
+	// 정수형 : char(1), short(2), int(4) long(4), long long(8) 
+	// 실수형 : float, double
+
+	int i = 0;   // 4 Byte 정수형 자료
+	
+
+
+
+	return 0;
+
+}
