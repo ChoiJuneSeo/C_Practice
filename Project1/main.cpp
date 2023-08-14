5403c76f05bd55bffab7115be86ca0c7fd9079e9
CJS_Labtop <cjs07310125@gmail.com>
2023-08-14 19:27:53 +0900

    문자열(1) 학습

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,6 +12,30 @@
 
 int main() {
 
+	// ¹®ÀÚ
+	// char(1) , wchar(2)
+
+	char c = 'a';
+	wchar_t wc = L'a';
+
+	char szChar[10] = "abcdef";
+
+	wchar_t szWChar[10] = L"abcdef";
+
+	short arrShort[10] = { 97,98,99,100,101,102, };
+
+	int a = 0;
+
+	const wchar_t* pChar = L"abcdef";
+
+	szChar[1] = 'z';
+	// pChar[1] = 'z';
+
+	
+
+
+
+
 
 	return 0;
 
