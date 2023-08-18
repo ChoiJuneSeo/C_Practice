5f78c7703b0934b2373ec3f272ed5aa1ecf56151
CJS_Labtop <cjs07310125@gmail.com>
2023-08-18 18:15:49 +0900

    동적할당 학습

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,7 +3,7 @@
 #include "common.h"
 #include <wchar.h>
 #include <assert.h>
-
+#include <iostream>
 
 //#define HUNGRY 0x01
 //#define THIRSTY 0x02
@@ -14,6 +14,30 @@
 
 
 int main() {
+
+	// 힙영역 ( 동적 할당 )
+	
+	// 동적 할당
+	// 1. 런타임 중에 대응 가능
+	// 2. 사용자가 직접 관리해야함(해제)
+
+
+	int* pInt = (int*)malloc(100);
+
+	float* pF = (float*)malloc(4);
+	int* pI = (int*)pF;
+
+	*pF = 2.4f;
+
+	if (nullptr != pInt)
+	{
+		free(pInt);
+	}
+
+	free(pF);
+
+
+
 
 	return 0;
 
