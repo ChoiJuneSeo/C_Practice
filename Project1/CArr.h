b14c01d51175814bd08b5a4b5650eab697ec0102
CJS_Labtop <cjs07310125@gmail.com>
2023-08-27 20:54:04 +0900

    함수 템플릿 클래스 템플릿 학습 CArr을 템플릿으로 변경

--- Project1/CArr.h
+++ Project1/CArr.h
@@ -1,18 +1,85 @@
 #pragma once
+#include <assert.h>
+
+
+template<typename T>
 class CArr
 {
 private:
-	int* m_pInt;
+	T* m_pData;
 	int m_iCount;
 	int m_iMaxCount;
 
 public:
-	void push_back(int _Data);
+	void push_back(const T& _Data);
 	void resize(int _iResizeCount);
 
-	int& operator[] (int idx);
+	T& operator[] (int idx);
 public:
 	CArr();
 	~CArr();
 };
+
+template<typename T>
+CArr<T>::CArr()
+	: m_pData(nullptr)
+	, m_iCount(0)
+	, m_iMaxCount(2)
+{
+	m_pData = new T[2];
+}
+template<typename T>
+CArr<T>::~CArr()
+{
+	delete[] m_pData;
+
+	/*	int* p = new int;
+		delete p;	*/
+}
+
+
+template<typename T>
+void CArr<T>::push_back(const T& _Data)
+{
+	if (m_iMaxCount <= m_iCount)
+	{
+		resize(m_iMaxCount * 2);
+	}
+
+	m_pData[m_iCount++] = _Data;
+}
+
+template<typename T>
+void CArr<T>::resize(int _iResizeCount)
+{
+	// 현재 최대 수용량 보다 더 적은 수치로 확장하려는 경우
+	if (m_iMaxCount >= _iResizeCount)
+	{
+		assert(nullptr);
+	}
+
+	// 1. 리사이즈 시킬 개수만큼 동적 할당 한다.
+	T* pNew = new T[_iResizeCount];
+
+	// 2. 기존 데이터들을 복사
+	for (int i = 0; i < m_iCount; ++i)
+	{
+		pNew[i] = m_pData[i];
+	}
+
+	// 3. 기존 공간 메모리 해제
+	delete[] m_pData;
+
+	// 4. 배열이 새로 할당된 공간을 가르키게 한다.
+	m_pData = pNew;
+
+	// 5. Maxcount 갱신
+	m_iMaxCount = _iResizeCount;
+
+}
+template<typename T>
+T& CArr<T>::operator[](int idx)
+{
+	return m_pData[idx];
+}
 
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,6 +2,24 @@
 #include "CArr.h"
 #include "myArr.h"
 
+
+//int Add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//float Add(float f, float f2)
+//{
+//	return f + f2;
+//}
+
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+
 int main()
 {
 	// 구조체 --> 클래스
@@ -32,7 +50,7 @@ int main()
 
 	ReleaseArr(&arr);
 
-	CArr cArr;
+	CArr<int> cArr;
 	
 	cArr.push_back(10);
 	cArr.push_back(20);
@@ -41,6 +59,16 @@ int main()
 	int a = cArr[1];
 	cArr[1] = 30;
 
+	// 함수 템플릿  ( 함수랑 동일취급 X )
+
+	int i = Add<int>(10, 20);
+
+	float f = Add<float>(10.f, 20.f);
+
+	// 클래스 템플릿
+	CArr<float> CArr2;
+	
+	CArr2.push_back(10.2f);
 
 	return 0;
 }
